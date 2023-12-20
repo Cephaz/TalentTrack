9b1a023e2ebdc1f79d6d1dbd7a36c294abd8e1ca
Pierre <zhou_p@etna-alternance.net>
2023-12-20 14:24:09 +0100

    - add stimulus notification - improve fr translation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,15 @@
 
 # Application helper
 module ApplicationHelper
+  # rubocop:disable Rails/OutputSafety
+  def show_svg(icon_name, options = {})
+    file = Rails.root.join('app', 'assets', 'images', "#{icon_name}.svg").read
+    doc = Nokogiri::HTML::DocumentFragment.parse file
+    svg = doc.at_css 'svg'
+
+    options.each { |attr, value| svg[attr.to_s] = value }
+
+    doc.to_html.html_safe
+  end
+  # rubocop:enable Rails/OutputSafety
 end
--- config/importmap.rb
+++ config/importmap.rb
@@ -7,3 +7,8 @@ pin '@hotwired/turbo-rails', to: 'turbo.min.js', preload: true
 pin '@hotwired/stimulus', to: 'stimulus.min.js', preload: true
 pin '@hotwired/stimulus-loading', to: 'stimulus-loading.js', preload: true
 pin_all_from 'app/javascript/controllers', under: 'controllers'
+
+# stimulus-notification
+pin "stimulus-notification", to: "https://ga.jspm.io/npm:stimulus-notification@2.2.0/dist/stimulus-notification.mjs"
+pin "hotkeys-js", to: "https://ga.jspm.io/npm:hotkeys-js@3.13.3/dist/hotkeys.esm.js"
+pin "stimulus-use", to: "https://ga.jspm.io/npm:stimulus-use@0.51.3/dist/index.js"
